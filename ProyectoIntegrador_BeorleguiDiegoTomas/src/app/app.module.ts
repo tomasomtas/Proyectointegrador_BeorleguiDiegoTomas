278b7d9569802c4d911eb896d7fd3d7671c010c9
tomasomtas <dbeorlegui@agro.uba.ar>
2023-01-15 14:30:24 -0300

    banner

--- ProyectoIntegrador_BeorleguiDiegoTomas/src/app/app.module.ts
+++ ProyectoIntegrador_BeorleguiDiegoTomas/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { LogoAPrComponent } from './components/logo-apr/logo-apr.component';
 import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SocialComponent } from './components/social/social.component';
     HeaderComponent,
     LogoAPComponent,
     LogoAPrComponent,
-    SocialComponent
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
